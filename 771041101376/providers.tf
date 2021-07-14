9d2f4acd07fd9fb76a5a069b476743c47b451f31
dudesang <87385255+dudesang@users.noreply.github.com>
2021-07-14 15:24:21 +0530

    Update providers.tf

--- 771041101376/providers.tf
+++ 771041101376/providers.tf
@@ -1,7 +1,11 @@
-provider "aws" {
-access_key = var.access_key
-secret_key = var.secret_key
-region     = var.region
-}
+##provider
+access_key = "AKIA3HBNBQZAIU2BVWWR"
+secret_key = "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"
+region     = "us-east-1"
 
+##vpc
+vpc_cidr             = "10.0.0.0/16"
+instance_tenancy     = "default"
+enable_dns_hostnames = "true"
+vpc_name             = "vpc-use1-ae-dev-test"
 
