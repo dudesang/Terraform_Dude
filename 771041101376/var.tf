c02039d393a456e684791315c73564c023f08d0d
dudesang <87385255+dudesang@users.noreply.github.com>
2021-07-14 15:26:28 +0530

    Create var.tf

---
+++ 771041101376/var.tf
@@ -0,0 +1,10 @@
+##provider
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+
+##vpc
+variable "vpc_cidr" {}
+variable "instance_tenancy" {}
+variable "enable_dns_hostnames" {}
+variable "vpc_name" {}
